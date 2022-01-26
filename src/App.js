e74db96d6a005ed443270eae2ddf1d693da5263f
Amira Salah <hotta44@hotmail.com>
2022-01-26 19:16:39 +0200

    start routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext, createContext, useState }  from 'react';
 import './App.css';
 import './theme/globalStyles.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
 import Login from './components/login';
 import Signup from './components/signup';
@@ -14,9 +23,8 @@ function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <h1>Home</h1>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<Products />} />
         <Route path="/profile" element={<AdminProfile />} />
