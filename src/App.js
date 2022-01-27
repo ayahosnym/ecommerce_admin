1fcac755e3740589fb698ff09260a60f34bde20f
TasneemGad <tasneemgad38@gmail.com>
2022-01-27 21:33:53 +0200

    Category UI

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ import Login from './components/login';
 import Signup from './components/signup';
 import AdminProfile from './components/adminProfile/adminProfile';
 import Products from "./components/products/productsList";
+import Categories from './components/categories/categoriesList';
+import Navbar from './components/NavBar/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
       <CssBaseline />
       <h1>Home</h1>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/profile" element={<AdminProfile />} />
+        <Route path="/Category" element={<Categories />} />
       </Routes>
     </div>
   );
